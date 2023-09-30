44d3c8368fcc3333ba3ba7bf630b0e134b88eae5
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-09-30 16:48:07 +0200

    fix: Don't require auth when dev-env

--- src/middleware/secure.middleware.ts
+++ src/middleware/secure.middleware.ts
@@ -1,7 +1,9 @@
 import type { NextFunction, Request, Response } from 'express';
+import { AppConfig } from '../app.config';
 
 export function secure(password: string) {
   return (req: Request, res: Response, next: NextFunction) => {
+    if (AppConfig.environment === 'DEV') return next();
     const pwd = req.query.password;
     if (!pwd) {
       return res.status(401).json({ message: "Provide an 'password' query-parameter" });
