a0ec12ea1b06169a11f6e24ed4da690ffb1f10d4
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-10-01 03:18:31 +0200

    feat: Provide standalone components

---
+++ public/components/chat/chat.js
@@ -0,0 +1,25 @@
+const socket = io();
+
+socket.on('chatMessage', async function (text, details, user) {
+  await appendChatMsg(user[0], text);
+});
+
+async function appendChatMsg(user, message) {
+  const msgId = Math.floor(Math.random() * 1001);
+  const messages = $('#chat-messages');
+  const msgContainer = $('#chat-msg-container');
+  msgContainer.prepend(`<p id="msg_${msgId}" class="chat-message" style="border: solid ${borderColor};">
+      <strong>${user}</strong><br />
+      <span>${message}</span>
+    </p>`);
+
+  if (msgContainer.outerHeight() > messages.outerHeight()) {
+    msgContainer.children(':last').fadeOut(1000);
+    await sleep(1000);
+    msgContainer.children(':last').remove();
+  }
+
+  $('#msg_' + msgId)
+    .hide()
+    .fadeIn(0);
+}
