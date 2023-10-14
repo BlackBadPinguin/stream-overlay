d4e3a88094881d372808adbf741cd88754ba931c
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-10-14 17:09:17 +0200

    refactor: Change message order and increase font-size

--- public/components/chat/chat.js
+++ public/components/chat/chat.js
@@ -8,15 +8,15 @@ async function appendChatMsg(user, message) {
   const msgId = Math.floor(Math.random() * 1001);
   const messages = $('#chat-messages');
   const msgContainer = $('#chat-msg-container');
-  msgContainer.prepend(`<p id="msg_${msgId}" class="chat-message">
+  msgContainer.append(`<p id="msg_${msgId}" class="chat-message">
       <strong>${user}</strong><br />
       <span>${message}</span>
     </p>`);
 
   if (msgContainer.outerHeight() > messages.outerHeight()) {
-    msgContainer.children(':last').fadeOut(1000);
+    msgContainer.children(':first').fadeOut(1000);
     await sleep(1000);
-    msgContainer.children(':last').remove();
+    msgContainer.children(':first').remove();
   }
 
   $('#msg_' + msgId)
